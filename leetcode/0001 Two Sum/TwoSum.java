89c582cba0192b12dd9f3f05d0475ae907a81ab1
Umang Gala <46402179+galaumang@users.noreply.github.com>
2022-07-07 13:58:11 +0530

    0001 Two Sum - java solution
    Add java solution for Two Sum problem

---
+++ leetcode/0001 Two Sum/TwoSum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (map.containsKey(nums[index]))
+                return new int[]{map.get(nums[index]), index};
+            else
+                map.put(target - nums[index], index);
+        }
+        return new int[]{-1, -1};
+    }
+}
