eb9c729ddbf69645066e7ec3a5788c2f57ff390f
Umang G <toumangg@gmail.com>
2023-03-20 20:11:53 +0530

    [LC-0543] Diameter of Binary Tree Add java solution for leetcode problem 543 - Diameter of Binary Tree

---
+++ solutions/lib/src/main/java/dsalgo/leetcode/BinaryTreeDiameter.java
@@ -0,0 +1,27 @@
+package dsalgo.leetcode;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+ }
+public class BinaryTreeDiameter {
+    int diameter;
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameter = 1;
+        depth(root);
+        return diameter - 1;
+    }
+
+    public int depth(TreeNode node) {
+        if (node == null)
+            return 0;
+        int left = depth(node.left);
+        int right = depth(node.right);
+        diameter = Math.max(diameter, left+right+1);
+        return Math.max(left,right) + 1;
+    }
+}
