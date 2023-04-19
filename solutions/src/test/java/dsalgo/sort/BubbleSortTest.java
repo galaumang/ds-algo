b45564c0d4ac801b092ec8de605dfe3ac9a09d04
Umang G <toumangg@gmail.com>
2023-04-19 15:47:21 +0530

    [Sorting] Tests for Sort algorithm Add unit tests for Bubble sort and Heap Sort algorithms and imports changes for Insertion Sort

--- solutions/src/test/java/dsalgo/sort/BubbleSortTest.java
+++ solutions/src/test/java/dsalgo/sort/BubbleSortTest.java
@@ -5,11 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class InsertionSortTest {
-    @ParameterizedTest(name = "Insertion Sort - {index} > {0} > ")
+public class BubbleSortTest {
+    @ParameterizedTest(name = "Bubble Sort - {index} > {0} > ")
     @MethodSource("dsalgo.sort.SortTestCases#testCases")
-    void insertionSortTest(int[] inputElements, int[] expected) {
-        InsertionSort.sort(inputElements);
+    void bubbleSortTest(int[] inputElements, int[] expected) {
+        BubbleSort.sort(inputElements);
         assertArrayEquals(expected, inputElements);
     }
 }
--- solutions/src/test/java/dsalgo/sort/HeapSortTest.java
+++ solutions/src/test/java/dsalgo/sort/HeapSortTest.java
@@ -5,11 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class InsertionSortTest {
-    @ParameterizedTest(name = "Insertion Sort - {index} > {0} > ")
+public class HeapSortTest {
+    @ParameterizedTest(name = "Heap Sort - {index} > {0} > ")
     @MethodSource("dsalgo.sort.SortTestCases#testCases")
-    void insertionSortTest(int[] inputElements, int[] expected) {
-        InsertionSort.sort(inputElements);
+    void heapSortTest(int[] inputElements, int[] expected) {
+        HeapSort.sort(inputElements);
         assertArrayEquals(expected, inputElements);
     }
 }
