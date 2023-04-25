3894aa7886a3e8faf3c3ae95f55f6fcf974eb291
Umang G <toumangg@gmail.com>
2023-04-25 07:39:46 +0530

    [Sorting] Shell Sort Test Fix the Parameterized test name for Shell sort test

--- solutions/src/test/java/dsalgo/sort/ShellSortTest.java
+++ solutions/src/test/java/dsalgo/sort/ShellSortTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class ShellSortTest {
-    @ParameterizedTest(name = "Merge Sort - {index} > {0} > ")
+    @ParameterizedTest(name = "Shell Sort - {index} > {0} > ")
     @MethodSource("dsalgo.sort.SortTestCases#testCases")
     void shellSortTest(int[] inputElements, int[] expected) {
         ShellSort.sort(inputElements);
