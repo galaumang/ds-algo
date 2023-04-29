26bace6dd9e3977ae180b7c6ebfe7b1feae2346e
Umang G <toumangg@gmail.com>
2023-04-29 08:01:04 +0530

    [LC-0001] Two Sum Add java solution with unit tests for leetcode problem 1 - Two Sum

---
+++ solutions-java/src/test/java/dsalgo/leetcode/TwoSumTest.java
@@ -0,0 +1,42 @@
+package dsalgo.leetcode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+/**
+ * TwoSumTest
+ *
+ * @author Umang G
+ */
+class TwoSumTest {
+    private static Stream<Arguments> testCases() {
+        return Stream.of(
+                Arguments.of(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 }),
+                Arguments.of(new int[] { 6, 2, 8, 4, 3 }, 18, new int[] { -1, -1 }),
+                Arguments.of(new int[] { 6, 2, 8, 4, 3 }, 12, new int[] { 2, 3 }),
+                Arguments.of(new int[] { 9 }, 2, new int[] { -1, -1 }));
+    }
+
+    @ParameterizedTest(name = "Brute Force Hash Table - {index} > {0}, {1} > ")
+    @MethodSource("testCases")
+    void testTwoSumBruteForce(int[] nums, int target, int[] expected) {
+        assertArrayEquals(expected, TwoSum.twoSumBruteForce(nums, target));
+    }
+
+    @ParameterizedTest(name = "One-pass Hash Table - {index} > {0}, {1} > ")
+    @MethodSource("testCases")
+    void testTwoSumOnePassHashTable(int[] nums, int target, int[] expected) {
+        assertArrayEquals(expected, TwoSum.twoSumOnePassHashTable(nums, target));
+    }
+
+    @ParameterizedTest(name = "Two-pass Hash Table - {index} > {0}, {1} > ")
+    @MethodSource("testCases")
+    void testTwoSumTwoPassHashTable(int[] nums, int target, int[] expected) {
+        assertArrayEquals(expected, TwoSum.twoSumTwoPassHashTable(nums, target));
+    }
+}
