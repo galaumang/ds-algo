54398415c458fe04460f97337241831662c6005c
Umang G <toumangg@gmail.com>
2023-05-01 10:15:37 +0530

    [LC-1426] Counting Elements Add java solution with unit tests for leetcode problem 1426 - Counting Elements

---
+++ solutions-java/src/main/java/dsalgo/leetcode/CountingElements.java
@@ -0,0 +1,33 @@
+package dsalgo.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CountingElements {
+    /**
+     * <p>
+     * {@code CountingElements} instance should NOT be constructed in standard
+     * programming, so its defined as private
+     * </p>
+     */
+    private CountingElements() {
+    }
+
+    /**
+     * countElements
+     * count the element x such that x + 1 element is present in the same array
+     *
+     * @param arr array elements
+     * @return count of x elements
+     */
+    public static int countElements(int[] arr) {
+        int count = 0;
+        Set<Integer> elements = new HashSet<>();
+        for (int element : arr)
+            elements.add(element);
+        for (int x : arr)
+            if (elements.contains(x + 1))
+                count++;
+        return count;
+    }
+}
---
+++ solutions-java/src/test/java/dsalgo/leetcode/CountingElementsTest.java
@@ -0,0 +1,30 @@
+package dsalgo.leetcode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * CountingElementsTest
+ *
+ * @author Umang G
+ */
+class CountingElementsTest {
+    private static Stream<Arguments> testCases() {
+        return Stream.of(
+                Arguments.of(new int[] { 1, 2, 3 }, 2),
+                Arguments.of(new int[] { 1, 1, 3, 3, 5, 5, 7, 7 }, 0),
+                Arguments.of(new int[] { 1, 3, 2, 3, 5, 0 }, 3),
+                Arguments.of(new int[] { 1, 1, 2, 2 }, 2));
+    }
+
+    @ParameterizedTest(name = "{index} > {0} > ")
+    @MethodSource("testCases")
+    void countElementsTest(int[] inputElements, int expected) {
+        assertEquals(expected, CountingElements.countElements(inputElements));
+    }
+}
