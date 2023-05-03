7ef34e7a263a8f6822752830b9dd5f6b6d8df834
Umang G <toumangg@gmail.com>
2023-05-03 16:29:12 +0530

    [LC-2178] Maximum Split of Positive Even Integers Add java solution for leetcode problem 2178 - Maximum Split of Positive Even Integers

---
+++ solutions-java/src/main/java/dsalgo/leetcode/MaximumEvenSplit.java
@@ -0,0 +1,24 @@
+package dsalgo.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MaximumEvenSplit {
+    public static List<Long> maximumEvenSplit(long finalSum) {
+        List<Long> result = new LinkedList<Long>();
+        if (finalSum % 2 == 1)
+            return result;
+        Long currSum = 0l;
+        Long currNum = 2l;
+        while (currSum < finalSum) {
+            currSum += currNum;
+            result.add(currNum);
+            currNum += 2;
+        }
+        if (currSum == finalSum)
+            return result;
+        else
+            result.remove(result.indexOf(currSum - finalSum));
+        return result;
+    }
+}
