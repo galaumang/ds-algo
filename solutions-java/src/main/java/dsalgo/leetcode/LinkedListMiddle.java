62eb9d0342c630e2be7e65cc8939cee56e7d9bfd
Umang G <toumangg@gmail.com>
2023-05-05 19:50:56 +0530

    LC-0876] Middle of the Linked List Add unit test with test cases for leetcode problem 0876 - Middle of the Linked List

---
+++ solutions-java/src/main/java/dsalgo/leetcode/LinkedListMiddle.java
@@ -0,0 +1,23 @@
+package dsalgo.leetcode;
+
+import dsalgo.util.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class LinkedListMiddle {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
