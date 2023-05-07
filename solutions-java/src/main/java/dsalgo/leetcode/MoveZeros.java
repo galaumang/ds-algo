8baa450ab86f6b1e67f5b2bbd5405723829751ca
Umang G <toumangg@gmail.com>
2023-05-07 08:45:59 +0530

    LC-0283] Move Zeros Add java solution for leetcode problem 283 - Move Zeros

---
+++ solutions-java/src/main/java/dsalgo/leetcode/MoveZeros.java
@@ -0,0 +1,13 @@
+package dsalgo.leetcode;
+
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        int nonZeroIndex = 0;
+        for (int num : nums) {
+            if (num != 0)
+                nums[nonZeroIndex++] = num;
+        }
+        while (nonZeroIndex < nums.length)
+            nums[nonZeroIndex++] = 0;
+    }
+}
