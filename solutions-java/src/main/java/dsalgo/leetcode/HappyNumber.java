ac4ba8ca188ca1bfbbad385e373e6ff865b44a53
Umang G <toumangg@gmail.com>
2023-05-08 09:14:14 +0530

    LC-0202] Happy Number Add java solution for leetcode problem 202 - Happy Number

---
+++ solutions-java/src/main/java/dsalgo/leetcode/HappyNumber.java
@@ -0,0 +1,23 @@
+package dsalgo.leetcode;
+
+public class HappyNumber {
+    int numSquareSum(int no) {
+        int squareSum = 0;
+        while (no != 0) {
+            int digit = no % 10;
+            squareSum += (digit * digit);
+            no /= 10;
+        }
+        return squareSum;
+    }
+
+    public boolean isHappy(int n) {
+        int slow, fast;
+        slow = fast = n;
+        do {
+            slow = numSquareSum(slow);
+            fast = numSquareSum(numSquareSum(fast));
+        } while (slow != fast);
+        return (slow == 1);
+    }
+}
