2e48a70df8cc4d3d6db371d173e2cce026f19caa
Umang G <toumangg@gmail.com>
2023-05-09 11:36:02 +0530

    [LC-0053] Maximum Subarray Add java solution for leetcode problem 53 - Maximum Subarray

---
+++ solutions-java/src/main/java/dsalgo/leetcode/MaximumSubarray.java
@@ -0,0 +1,15 @@
+package dsalgo.leetcode;
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int current = Integer.MIN_VALUE;
+        int maximum = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            current = Math.max(0, current);
+            current += num;
+            maximum = Math.max(maximum, current);
+        }
+        return maximum;
+    }
+}
