b47b822fdc3f80274cbf8d60aec6197ee7317ff7
Umang G <toumangg@gmail.com>
2023-05-11 07:51:58 +0530

    [LC-0001] Two Sum Add cpp solution for leetcode problem 1 - Two Sum

---
+++ solutions-cpp/src/main/cpp/dsalgo/leetcode/two_sum.cpp
@@ -0,0 +1,24 @@
+#include <vector>;
+#include <map>;
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        map<int, int> map;
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (map.find(target - nums[i]) != map.end())
+            {
+                ans.push_back(map[target - nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            map[nums[i]] = i;
+        }
+        return ans;
+    }
+};
