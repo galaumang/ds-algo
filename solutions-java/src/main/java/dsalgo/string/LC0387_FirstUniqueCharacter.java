4c3d16c7a21600ee51c89125eccf7d41e11590b7
Umang Gala <toumangg@gmail.com>
2023-10-12 19:48:43 +0530

    feat: LC-0387: First Unique Character in a String add java solution for leetcode problem 387 - First Unique Character in a String

---
+++ solutions-java/src/main/java/dsalgo/string/LC0387_FirstUniqueCharacter.java
@@ -0,0 +1,16 @@
+package dsalgo.string;
+
+public class LC0387_FirstUniqueCharacter {
+  public int firstUniqChar(String s) {
+    int[] hash = new int[26];
+    int n = s.length();
+    for (int i = 0; i < n; i++) {
+      hash[s.charAt(i) - 'a']++;
+    }
+    for (int i = 0; i < n; i++) {
+      if (hash[s.charAt(i) - 'a'] == 1)
+        return i;
+    }
+    return -1;
+  }
+}
