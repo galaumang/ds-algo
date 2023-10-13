91ecc19d1bc3cee23360467a73af72dcef6403e6
Umang Gala <toumangg@gmail.com>
2023-10-13 16:42:37 +0530

    feat: LC-0001: Two Sum (#1) refactor the test to include parameterized test cases and add dependencies list with required libraries

--- solutions-py/src/test/array/test_LC_0001_two_sum.py
+++ solutions-py/src/test/array/test_LC_0001_two_sum.py
@@ -1,16 +1,21 @@
 import unittest
 
+from parameterized import parameterized
+from typing import List
+
 from src.main.array.LC_0001_two_sum import TwoSum
 
 class TestTwoSum(unittest.TestCase):
   def setUp(self) -> None:
      self.object = TwoSum()
-  
-  def test_two_sum(self):
-    nums = [2,7,11,15]
-    target = 9
-    result = self.object.twoSum(nums, target)
-    self.assertListEqual(result, [0,1])
+
+  @parameterized.expand([
+     ([2,7,11,15], 9, [0,1]),
+     ([3,2,4], 6, [1,2]),
+     ([3,3], 6, [0,1])
+  ])
+  def tests_two_sum(self, nums: List[int], target: int, expected: List[int]):
+     self.assertListEqual(expected, self.object.twoSum(nums, target))
 
 if __name__ == '__main__':
     unittest.main()
